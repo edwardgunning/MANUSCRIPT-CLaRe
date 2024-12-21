1da2833cbbf96782b9410e97be3f0869883f55c7
Your Name <edward.gunning@ul.ie>
2024-12-20 20:20:04 -0500

    add sample size experiments.

---
+++ sample-size-experiment.R
@@ -0,0 +1,91 @@
+library(GLarE)
+
+tensorflow::set_random_seed(1)
+eye <- as.matrix(glaucoma_data)
+
+sample_sizes <- round(306/ (2^seq(0, 3, by = 1)))
+
+inds_list <- glare_list <- vector("list", length = length(sample_sizes))
+
+par(mfrow = c(1, 4))
+
+for(i in seq_along(sample_sizes)) {
+  print(paste("Sample size =", sample_sizes[i]))
+  inds_list[[i]] <- inds <- sample(1:306, replace = FALSE, size = sample_sizes[i])
+  mat_i <- eye[inds, ]
+  glare_list[[i]] <- GLaRe(mat = mat_i)
+}
+
+
+
+summary_correlation_plot_custom <- function(out_basisel, cvqlines, cutoff_criterion, r, q, breaks, method_name, qc, tolerance_level) {
+  correlation_df <- GLarE:::transform_correlation_output(out_basisel, cvqlines, cutoff_criterion)
+  plot(
+    x = breaks,
+    y = correlation_df[, "meansqcor_t"],
+    type = "b",
+    pch = 20,
+    col = "green",
+    lwd = 3,
+    lty = 1,
+    panel.first = c(abline(h = 0, lty = 1, col = "black"), abline(h = 1, lty = 1, col = "black")),
+    xlab = "No. of Latent Features",
+    ylab = expression(paste("Loss: 1 - Squared Correlation (", 1 - R^2, ")", sep = "")),
+    xlim = range(glare_list[[1]]$breaks),
+    ylim = c(0, 1),
+    main = paste("Latent Feature Representation \n Summary:", method_name)
+  )
+  lines(x = breaks[seq_len(r)], correlation_df[seq_len(r), "minsqcor_cv"], col = "royalblue", lwd = 2, type = "b", pch = 20)
+  lines(x = breaks[seq_len(r)], correlation_df[seq_len(r), "meansqcor_cv"], col = "goldenrod", lwd = 2, type = "b", pch = 20)
+  lines(x = breaks[seq_len(r)], correlation_df[seq_len(r), "maxsqcor_cv"], col = "red3", lwd = 2, type = "b", pch = 20)
+  if (cvqlines == 0.5) {
+    lines(x = breaks[seq_len(r)], correlation_df[seq_len(r), "medsqcor_cv"], col = "purple", lwd = 2, type = "b", pch = 20)
+  } else if (cvqlines != 0.5) {
+    lines(x = breaks[seq_len(r)], correlation_df[seq_len(r), "qchoice_cv"], col = "purple", lwd = 2, type = "b", pch = 20)
+  }
+  lines(x = breaks[seq_len(r)], correlation_df[seq_len(r), "cutoff_criterion_cv"], col = "grey", lwd = 2, lty = 2, type = "b", pch = 20)
+
+  if (!is.na(qc)) {
+    abline(v = qc, lty = 2, col = "grey")
+    abline(h = tolerance_level, lty = 2, col = "grey")
+    axis(side = 1, at = c(qc), labels = paste0("qc = ", qc), col = "darkgrey", font = 4, lwd = 3, padj = 1.2)
+    axis(side = 2, at = c(tolerance_level), labels = paste0("ε = ", tolerance_level), col = "darkgrey", font = 4, lwd = 3, padj = 1.2)
+  }
+
+
+  legend("topright",
+         legend = c(
+           "CV Min Loss",
+           "CV Mean Loss",
+           paste("CV Percentile =", cvqlines, "Loss"),
+           "CV Max Loss",
+           "Training Mean Loss",
+           paste("Cut-Off Criterion = ", cutoff_criterion, "Loss")
+         ),
+         col = c("blue", "goldenrod", "purple", "red3", "green", "grey"),
+         lty = c(1, 1, 1, 1, 1, 2),
+         lwd = c(2, 2, 2, 2, 2, 2, 2),
+         bg = "white"
+  )
+}
+
+
+
+cairo_pdf(file = "figures/eye-sample-size-results-results-01.pdf", width = 15, height = 15/4, family="DejaVu Sans")
+par(mfrow = c(1, 4), mar=c(5,6,4,1), cex = 0.75)
+for(j in 1:4) {
+  summary_correlation_plot_custom(glare_list[[j]],
+                                   cvqlines = 0.9,
+                                   cutoff_criterion = 0.95,
+                                   tolerance_level = 0.05,
+                                   method_name = paste0("N = ", sample_sizes[j]),
+                                   r = glare_list[[j]]$r,
+                                   q = glare_list[[j]]$q,
+                                   breaks = glare_list[[j]]$breaks,
+                                   qc = glare_list[[j]]$qc)
+}
+
+dev.off()
+
+
+summary_correlation_plot_custom(glare_list[[1]], cvqlines = )
