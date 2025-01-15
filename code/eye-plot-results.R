6310b6f11050765823c22138c4a59cd3342caedd
Your Name <edward.gunning@ul.ie>
2025-01-15 09:50:41 -0500

    update eye plot results

--- code/eye-plot-results.R
+++ code/eye-plot-results.R
@@ -9,32 +9,32 @@ cairo_pdf(file = "figures/eye-results.pdf", width = 12, height = 4, family="Deja
 par(mfrow = c(1, 3), mar=c(5,6,4,1))
 GLarE:::summary_correlation_plot(eye_results$glare$pca,
                                  cvqlines = 0.9,
-                                 cutoff_criterion = 0.95,
+                                 attainment_rate = 0.95,
                                  tolerance_level = 0.05,
                                  method_name = "(a) PCA",
                                  r = eye_results$glare$pca$r,
                                  q = eye_results$glare$pca$r,
                                  breaks = eye_results$glare$pca$breaks,
-                                 qc = eye_results$glare$pca$qc)
+                                 qd = eye_results$glare$pca$qd)
 
 GLarE:::summary_correlation_plot(eye_results$glare$dwt,
                                  cvqlines = 0.9,
-                                 cutoff_criterion = 0.95,
+                                 attainment_rate = 0.95,
                                  tolerance_level = 0.05,
                                  method_name = "(b) DWT",
                                  r = eye_results$glare$dwt$r,
                                  q = eye_results$glare$dwt$r,
                                  breaks = eye_results$glare$dwt$breaks,
-                                 qc = eye_results$glare$dwt$qc)
+                                 qd = eye_results$glare$dwt$qd)
 
 
 GLarE:::summary_correlation_plot(eye_results$glare$ae,
                                  cvqlines = 0.9,
-                                 cutoff_criterion = 0.95,
+                                 attainment_rate = 0.95,
                                  tolerance_level = 0.05,
                                  method_name = "(c) AE",
                                  r = eye_results$glare$ae$r,
                                  q = eye_results$glare$ae$r,
                                  breaks = eye_results$glare$ae$breaks,
-                                 qc = eye_results$glare$ae$qc)
+                                 qd = eye_results$glare$ae$qd)
 dev.off()
