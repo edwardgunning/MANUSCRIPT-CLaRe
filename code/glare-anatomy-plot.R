9789baef5fcabe9bba0efa705d31c0f446d6c819
Your Name <edward.gunning@ul.ie>
2025-01-15 07:32:18 -0500

    update glare anatomy plot

--- code/glare-anatomy-plot.R
+++ code/glare-anatomy-plot.R
@@ -7,13 +7,13 @@ eye <- as.matrix(glaucoma_data)
 eye_pca <- GLaRe(mat = eye, learn = "pca", latent_dim_by = 10)
 
 cairo_pdf(file = "figures/glare-anatomy-plot.pdf", width = 7, height = 7, family="DejaVu Sans")
-par(mfrow = c(1,1), mar=c(5,6,4,1))
+par(mfrow = c(1,1), mar=c(5,6,4,1), cex = 1.25)
 GLarE:::summary_correlation_plot(out_basisel = eye_pca,
                                  cvqlines = 0.9,
-                                 cutoff_criterion = 0.95,
+                                 attainment_rate = 0.95,
                                  tolerance_level = 0.05,
                                  r = eye_pca$r, q = eye_pca$q,
-                                 qc = eye_pca$qc,
+                                 qd = eye_pca$qd,
                                  breaks = eye_pca$breaks,
                                  method_name = "Demo of PCA on Glaucoma Data")
 dev.off()
